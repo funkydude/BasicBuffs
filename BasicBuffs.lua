28df299be3db80b53d9077a87c679ff856ad5c74
funkydude <funkydude@users.noreply.github.com>
2016-09-15 15:22:38 +0100

    Shuffle code around

--- BasicBuffs.lua
+++ BasicBuffs.lua
@@ -1,33 +1,43 @@
 
 local f = CreateFrame("Frame", "BasicBuffsFrame", UIParent)
+
+local bg = f:CreateTexture()
+bg:SetAllPoints(f)
+bg:SetColorTexture(0, 1, 0, 0.3)
+
+local setBuff = BuffFrame.SetPoint
+BuffFrame:ClearAllPoints()
+setBuff(BuffFrame, "TOPRIGHT", f, "TOPRIGHT")
+hooksecurefunc(BuffFrame, "SetPoint", function(frame)
+	frame:ClearAllPoints()
+	setBuff(frame, "TOPRIGHT", f, "TOPRIGHT")
+end)
+
+f:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+f:SetWidth(280)
+f:SetHeight(225)
+f:Show()
+f:EnableMouse(true)
+f:RegisterForDrag("LeftButton")
+f:SetMovable(true)
+f:SetScript("OnDragStart", function(frame) frame:StartMoving() end)
+f:SetScript("OnDragStop", function(frame)
+	frame:StopMovingOrSizing()
+	local s = frame:GetEffectiveScale()
+	BasicBuffsStorage.x = frame:GetLeft() * s
+	BasicBuffsStorage.y = frame:GetTop() * s
+end)
+
 f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function(display)
 	if not BasicBuffsStorage then
 		BasicBuffsStorage = {}
 	end
 
-	display:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-	display:SetWidth(280)
-	display:SetHeight(225)
-	display:Show()
-	display:EnableMouse(true)
-	display:RegisterForDrag("LeftButton")
-	display:SetMovable(true)
-	display:SetScript("OnDragStart", function(frame) frame:StartMoving() end)
-	display:SetScript("OnDragStop", function(frame)
-		frame:StopMovingOrSizing()
-		local s = frame:GetEffectiveScale()
-		BasicBuffsStorage.x = frame:GetLeft() * s
-		BasicBuffsStorage.y = frame:GetTop() * s
-	end)
-	local bg = display:CreateTexture()
-	bg:SetAllPoints(display)
-	bg:SetColorTexture(0, 1, 0, 0.3)
-
 	if BasicBuffsStorage.x and BasicBuffsStorage.y then
 		local s = display:GetEffectiveScale()
 		display:ClearAllPoints()
-		display:SetPoint("TOPLEFT", "UIParent", "BOTTOMLEFT", BasicBuffsStorage.x / s, BasicBuffsStorage.y / s)
+		display:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", BasicBuffsStorage.x / s, BasicBuffsStorage.y / s)
 	end
 
 	if BasicBuffsStorage.lock then
@@ -36,31 +46,26 @@ f:SetScript("OnEvent", function(display)
 		display:SetMovable(false)
 	end
 
-	local setBuff = BuffFrame.SetPoint
-	BuffFrame:ClearAllPoints()
-	setBuff(BuffFrame, "TOPRIGHT", display, "TOPRIGHT")
-	hooksecurefunc(BuffFrame, "SetPoint", function(frame)
-		frame:ClearAllPoints()
-		setBuff(frame, "TOPRIGHT", display, "TOPRIGHT")
-	end)
-
-	SlashCmdList.BASICBUFFS = function()
-		if not BasicBuffsStorage.lock then
-			bg:Hide()
-			display:EnableMouse(false)
-			display:SetMovable(false)
-			BasicBuffsStorage.lock = true
-			print("|cFF33FF99BasicBuffs|r:", _G.LOCKED)
-		else
-			bg:Show()
-			display:EnableMouse(true)
-			display:SetMovable(true)
-			BasicBuffsStorage.lock = nil
-			print("|cFF33FF99BasicBuffs|r:", _G.UNLOCK)
-		end
-	end
-	SLASH_BASICBUFFS1 = "/basicbuffs"
 	display:UnregisterEvent("PLAYER_LOGIN")
 	display:SetScript("OnEvent", nil)
 end)
+
+SlashCmdList.BASICBUFFS = function()
+	if not BasicBuffsStorage then return end
+
+	if not BasicBuffsStorage.lock then
+		bg:Hide()
+		f:EnableMouse(false)
+		f:SetMovable(false)
+		BasicBuffsStorage.lock = true
+		print("|cFF33FF99BasicBuffs|r:", _G.LOCKED)
+	else
+		bg:Show()
+		f:EnableMouse(true)
+		f:SetMovable(true)
+		BasicBuffsStorage.lock = nil
+		print("|cFF33FF99BasicBuffs|r:", _G.UNLOCK)
+	end
+end
+SLASH_BASICBUFFS1 = "/basicbuffs"
 
